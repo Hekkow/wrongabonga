5307186dcfce7f5364bacd474a074978775d8e0c
plutothepenguin <theomidghafori@gmail.com>
2021-04-21 16:06:05 -0400

    added parallax, title, a couple images

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
+document.addEventListener("DOMContentLoaded", function(event) {
+  window.scrollTo(0, 0);
+});
+
+function parallax() {
+  document.getElementById("ashe").style.top = 130 + (0 - window.pageYOffset/400) + "%";
+  document.getElementById("teemo").style.top = 230 + (0 - window.pageYOffset/400) + "%";
+  document.getElementById("lee").style.top = 330 + (0 - window.pageYOffset/400) + "%";
+  document.getElementById("floater3").style.top = 150 + (0 - window.pageYOffset/25) + "%";
+  document.getElementById("bf").style.top = 180 + (0 - window.pageYOffset/8) + "%";
 }
+
+window.addEventListener("scroll", function(){
+	parallax();
+});
